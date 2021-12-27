47427eb5a72124b5200df386952a436db1877546
arvindsingh99 <singharvi69@gmail.com>
2021-12-27 07:41:41 +0530

    added

---
+++ Array/21.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+
+class Demo {
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter no of elements in array: ");
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    System.out.println("Enter " + n + " elements of array: ");
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+    for (int i = 0; i < n; i++) {
+      int count = 0;
+      for (int j=0; j<n; j++) {
+          if(a[i]==a[j]){
+            count++;
+          }
+         }
+         if(count==1){
+           System.out.print(a[i]);
+         }
+    }
+  }
+}
